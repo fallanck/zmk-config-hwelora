14111b49d984924263aad73d4ab1078d79af3e1b
MoltenZaz <mkenneththomas@gmail.com>
2025-02-10 17:18:46 +1100

    Initial shield

---
+++ src/hid_layers.c
@@ -0,0 +1,12 @@
+static int raw_hid_received_event_listener(const zmk_event_t *eh) {
+    struct raw_hid_received_event *event = as_raw_hid_received_event(eh);
+    if (event) {
+        // do something
+		zmk_keymap_layer_to(4);
+    }
+
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(process_raw_hid_event, raw_hid_received_event_listener);
+ZMK_SUBSCRIPTION(process_raw_hid_event, raw_hid_received_event);
